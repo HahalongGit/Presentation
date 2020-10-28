43ef5ed244a79972d865f20cdc1409d7f559f38a
LonglongLiu <longlong.liu@android.com>
2020-10-28 09:49:38 +0800

    添加DisplayManager方式双屏异显

--- app/src/main/java/com/digua/displayapp/MainActivity.java
+++ app/src/main/java/com/digua/displayapp/MainActivity.java
@@ -1,223 +1,42 @@
 package com.digua.displayapp;
 
-import android.app.Presentation;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.res.Resources;
-import android.media.MediaRouter;
-import android.opengl.GLSurfaceView;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.Button;
 
+import com.digua.displayapp.way1.PresentationWithMediaRouterActivity;
+import com.digua.displayapp.way2.PresentationDisplayManagerActivity;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String TAG = "PresentationActivity";
+    @BindView(R.id.btn_mediaRouter)
+    Button mBtnMediaRouter;
+    @BindView(R.id.btn_displayManager)
+    Button mBtnDisplayManager;
 
-    private MediaRouter mMediaRouter;
-    private DemoPresentation mPresentation;
-    private GLSurfaceView mSurfaceView;
-    private TextView mInfoTextView;
-    private boolean mPaused;
-
-    /**
-     * Initialization of the Activity after it is first created.  Must at least
-     * call {@link android.app.Activity#setContentView setContentView()} to
-     * describe what is to be displayed in the screen.
-     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Be sure to call the super class.
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Get the media router service.
-        mMediaRouter = (MediaRouter)getSystemService(Context.MEDIA_ROUTER_SERVICE);
-
-        // See assets/res/any/layout/presentation_with_media_router_activity.xml for this
-        // view layout definition, which is being set here as
-        // the content of our screen.
-        setContentView(R.layout.presentation_with_media_router_activity);
-
-        // Set up the surface view for visual interest.
-        mSurfaceView = findViewById(R.id.surface_view);
-        mSurfaceView.setRenderer(new CubeRenderer(false));
-
-        // Get a text view where we will show information about what's happening.
-        mInfoTextView = findViewById(R.id.info);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
-    @Override
-    protected void onResume() {
-        // Be sure to call the super class.
-        super.onResume();
-
-        // Listen for changes to media routes.
-        mMediaRouter.addCallback(MediaRouter.ROUTE_TYPE_LIVE_VIDEO, mMediaRouterCallback);
-
-        // Update the presentation based on the currently selected route.
-        mPaused = false;
-        updatePresentation();
-    }
-
-    @Override
-    protected void onPause() {
-        // Be sure to call the super class.
-        super.onPause();
-
-        // Stop listening for changes to media routes.
-        mMediaRouter.removeCallback(mMediaRouterCallback);
-
-        // Pause rendering.
-        mPaused = true;
-        updateContents();
-    }
-
-    @Override
-    protected void onStop() {
-        // Be sure to call the super class.
-        super.onStop();
-        // Dismiss the presentation when the activity is not visible.
-        if (mPresentation != null) {
-            Log.i(TAG, "Dismissing presentation because the activity is no longer visible.");
-            mPresentation.dismiss();
-            mPresentation = null;
-        }
-    }
-
-    private void updatePresentation() {
-        // Get the current route and its presentation display.
-        MediaRouter.RouteInfo route = mMediaRouter.getSelectedRoute(
-                MediaRouter.ROUTE_TYPE_LIVE_VIDEO);
-        Display presentationDisplay = route != null ? route.getPresentationDisplay() : null;
-
-        // Dismiss the current presentation if the display has changed.
-        if (mPresentation != null && mPresentation.getDisplay() != presentationDisplay) {
-            Log.i(TAG, "Dismissing presentation because the current route no longer "
-                    + "has a presentation display.");
-            mPresentation.dismiss();
-            mPresentation = null;
-        }
-
-        // Show a new presentation if needed.
-        if (mPresentation == null && presentationDisplay != null) {
-            Log.i(TAG, "Showing presentation on display: " + presentationDisplay);
-            mPresentation = new DemoPresentation(this, presentationDisplay);
-            mPresentation.setOnDismissListener(mOnDismissListener);
-            try {
-                mPresentation.show();
-            } catch (WindowManager.InvalidDisplayException ex) {
-                Log.w(TAG, "Couldn't show presentation!  Display was removed in "
-                        + "the meantime.", ex);
-                mPresentation = null;
-            }
-        }
-
-        // Update the contents playing in this activity.
-        updateContents();
-    }
-
-    private void updateContents() {
-        // Show either the content in the main activity or the content in the presentation
-        // along with some descriptive text about what is happening.
-        if (mPresentation != null) {
-            mInfoTextView.setText(getResources().getString(
-                    R.string.presentation_with_media_router_now_playing_remotely,
-                    mPresentation.getDisplay().getName()));
-            mSurfaceView.setVisibility(View.INVISIBLE);
-            mSurfaceView.onPause();
-            if (mPaused) {
-                mPresentation.getSurfaceView().onPause();
-            } else {
-                mPresentation.getSurfaceView().onResume();
-            }
-        } else {
-            mInfoTextView.setText(getResources().getString(
-                    R.string.presentation_with_media_router_now_playing_locally,
-                    getWindowManager().getDefaultDisplay().getName()));
-            mSurfaceView.setVisibility(View.VISIBLE);
-            if (mPaused) {
-                mSurfaceView.onPause();
-            } else {
-                mSurfaceView.onResume();
-            }
-        }
-    }
-
-    private final MediaRouter.SimpleCallback mMediaRouterCallback =
-            new MediaRouter.SimpleCallback() {
-                @Override
-                public void onRouteSelected(MediaRouter router, int type, MediaRouter.RouteInfo info) {
-                    Log.d(TAG, "onRouteSelected: type=" + type + ", info=" + info);
-                    updatePresentation();
-                }
-
-                @Override
-                public void onRouteUnselected(MediaRouter router, int type, MediaRouter.RouteInfo info) {
-                    Log.d(TAG, "onRouteUnselected: type=" + type + ", info=" + info);
-                    updatePresentation();
-                }
-
-                @Override
-                public void onRoutePresentationDisplayChanged(MediaRouter router, MediaRouter.RouteInfo info) {
-                    Log.d(TAG, "onRoutePresentationDisplayChanged: info=" + info);
-                    updatePresentation();
-                }
-            };
-
-    /**
-     * Listens for when presentations are dismissed.
-     */
-    private final DialogInterface.OnDismissListener mOnDismissListener =
-            new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    if (dialog == mPresentation) {
-                        Log.i(TAG, "Presentation was dismissed.");
-                        mPresentation = null;
-                        updateContents();
-                    }
-                }
-            };
-
-    /**
-     * The presentation to show on the secondary display.
-     * <p>
-     * Note that this display may have different metrics from the display on which
-     * the main activity is showing so we must be careful to use the presentation's
-     * own {@link Context} whenever we load resources.
-     * </p>
-     */
-    private final static class DemoPresentation extends Presentation {
-        private GLSurfaceView mSurfaceView;
-
-        public DemoPresentation(Context context, Display display) {
-            super(context, display);
-        }
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            // Be sure to call the super class.
-            super.onCreate(savedInstanceState);
-
-            // Get the resources for the context of the presentation.
-            // Notice that we are getting the resources from the context of the presentation.
-            Resources r = getContext().getResources();
-
-            // Inflate the layout.
-            setContentView(R.layout.presentation_with_media_router_content);
-
-            // Set up the surface view for visual interest.
-            mSurfaceView = (GLSurfaceView)findViewById(R.id.surface_view);
-            mSurfaceView.setRenderer(new CubeRenderer(false));
-        }
-
-        public GLSurfaceView getSurfaceView() {
-            return mSurfaceView;
+    @OnClick({R.id.btn_mediaRouter, R.id.btn_displayManager})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_mediaRouter:
+                startActivity(new Intent(this, PresentationWithMediaRouterActivity.class));
+                break;
+            case R.id.btn_displayManager:
+                startActivity(new Intent(this, PresentationDisplayManagerActivity.class));
+                break;
         }
     }
 }
--- app/src/main/java/com/digua/displayapp/way1/Cube.java
+++ app/src/main/java/com/digua/displayapp/way1/Cube.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.digua.displayapp;
+package com.digua.displayapp.way1;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
